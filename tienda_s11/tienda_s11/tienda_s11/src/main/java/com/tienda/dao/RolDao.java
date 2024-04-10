e25bc80c663338c93a2cb174d04beb734aa7cd8c
Royvin <royvin1205@gmail.com>
2024-04-10 11:02:44 -0600

    Revert "[S11]"
    This reverts commit 86d30815d5329b5b8d5ed250d1d0b564fbad58e7.

--- tienda_s11/tienda_s11/tienda_s11/src/main/java/com/tienda/dao/RolDao.java
+++
@@ -1,8 +0,0 @@
-package com.tienda.dao;
-
-import com.tienda.domain.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolDao extends JpaRepository<Rol, Long> {
-
-}
